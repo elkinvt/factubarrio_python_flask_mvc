1edc7a140ffc3ecb91672bf7f21a4771322e123e
elkin <filasachi@gmail.com>
2024-08-17 18:44:36 -0500

    se relaciona y deja funcionando todas las paginas de clientes y boton ' cerrar_sesion'

--- src/app.py
+++ src/app.py
@@ -13,3 +13,19 @@ def index():
 @app.route('/pagina_pricipal')
 def pagina_principal():
     return render_template('pgprincipal.html')
+
+@app.route('/clientes_crear')
+def clientes_crear():
+    return render_template('form_crear_cliente.html')
+
+@app.route('/clientes_ver')
+def clientes_ver():
+    return render_template('form_ver_cliente.html')
+
+@app.route('/clientes_editar')
+def clientes_editar():
+    return render_template('form_editar_cliente.html')
+
+@app.route('/cerrar_sesion')
+def cerrar_sesion():
+    return render_template('index.html')
