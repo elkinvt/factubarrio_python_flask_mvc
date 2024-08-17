90f3c1c77da10d66412ca34a61e55ed9a224abbf
elkin <filasachi@gmail.com>
2024-08-16 22:35:59 -0500

    se incorpora la carpeta 'docs' y se crea la carpeta 'static' para que funcione la imagen, y se deja funcioanndo el redireccionamiento a index

--- src/app.py
+++ src/app.py
@@ -1,1 +1,12 @@
-from flask import flask
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder='docs')
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+@app.route('/')
+
+def index():
+    return render_template('index.html')
